56e676758c76884f402c970154151f8e9b0be419
Mykyta Rostotskiy <rostotskiy.n@gmail.com>
2024-01-26 03:44:44 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+let text1 = prompt("Enter text1");
+let text2 = prompt("Enter text2");
+let text3 = prompt("Enter text3");
+alert(text1 + " " + text2 + " " + text3);
+alert("5" + " " + "4" + " " + "3" + " " + "2" + " " + "1");
